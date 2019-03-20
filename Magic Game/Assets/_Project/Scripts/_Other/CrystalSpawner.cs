f9cb3919f24c0b8307b04ebe99a4b0ab38a7330f
TuomoHei <31925862+TuomoHei@users.noreply.github.com>
2019-03-20 13:03:30 +0200

    CrystalSpawner-script added

---
+++ Magic Game/Assets/_Project/Scripts/_Other/CrystalSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalSpawner : MonoBehaviour
+{
+    //Prefab of crystal goes here
+    public GameObject crystalPrefab;
+
+    //List of empty game objects where crystal can spawn
+    public List<Transform> spawnLocations = new List<Transform>();
+
+    Transform newPosition;
+
+    void Start()
+    {
+        //Position of crystal randomly picked from list of spawn locations
+        newPosition = spawnLocations[Random.Range(0, spawnLocations.Count)];
+
+        //Spawn crystal
+        GameObject crystalSpawn = Instantiate(crystalPrefab, newPosition) as GameObject;
+    }
+}
