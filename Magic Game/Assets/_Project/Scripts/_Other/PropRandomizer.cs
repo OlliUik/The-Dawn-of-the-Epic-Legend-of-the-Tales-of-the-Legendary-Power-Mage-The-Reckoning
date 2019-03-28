a3d766218e0db4bbac4fa8c8fdcbc2072ba8c7fc
TuomoHei <31925862+TuomoHei@users.noreply.github.com>
2019-03-28 15:53:46 +0200

    New CrystalSpawner- and PropRandomizer-scripts

--- Magic Game/Assets/_Project/Scripts/_Other/PropRandomizer.cs
+++ Magic Game/Assets/_Project/Scripts/_Other/PropRandomizer.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class PropRandomizer : MonoBehaviour
 {
     //Prefabs of props goes here
-    public List<GameObject> propPrefabs = new List<GameObject>();
+    [SerializeField]
+    private List<GameObject> propPrefabs = new List<GameObject>();
 
-    //List of empty game objects where props can spawn
-    public List<Transform> spawnLocations = new List<Transform>();
+    //List of vectors where props can spawn
+    [SerializeField]
+    private Vector3[] spawns;
+
+    [SerializeField, Range(0, 1)]
+    private float spawnPercent;
 
     //List of random number order of spawn location count
     List<int> randomOrder = new List<int>();
 
-    Transform newPosition;
+    Vector3 newPosition;
 
     void Start()
     {
-        for (int n = 0; n < spawnLocations.Count; n++)
+        for (int n = 0; n < spawns.Length; n++)
         {
             randomOrder.Add(n);
         }
@@ -35,13 +40,32 @@ public class PropRandomizer : MonoBehaviour
             int index = Random.Range(0, randomOrder.Count);
             int i = randomOrder[index];
 
-            //Position of props randomly picked from list of spawn locations
-            newPosition = spawnLocations[randomOrder[index]];
+            if (Random.value < spawnPercent)
+            {
+                //Position of props randomly picked from list of spawn locations
+                newPosition = spawns[randomOrder[index]];
 
-            //Spawn prop
-            GameObject newProp = Instantiate(propPrefabs[Random.Range(0, propPrefabs.Count)], newPosition) as GameObject;
+                //Add spawner's position to Vector3
+                newPosition = newPosition + gameObject.transform.position;
+
+                //Spawn prop
+                GameObject newProp = Instantiate(propPrefabs[Random.Range(0, propPrefabs.Count)], newPosition, Quaternion.identity) as GameObject;
+
+                //Make spawner as crystal's parent
+                newProp.transform.parent = gameObject.transform;
+            }
 
             randomOrder.RemoveAt(index);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+
+        foreach (Vector3 spawn in spawns)
+        {
+            Gizmos.DrawWireCube(spawn, new Vector3(1, 1, 1));
         }
     }
 }
