5151fecc93ba5fd8ca977f36fae51caca59e7eaf
AJ-Ora <antti-jussioravainen@kamk.fi>
2019-04-10 10:12:26 +0300

    Made some changes to homing projectiles.

--- Magic Game/Assets/_Project/Scripts/SpellSystem/SpellModifiers/ModifierScripts/Projectile/Homing.cs
+++ Magic Game/Assets/_Project/Scripts/SpellSystem/SpellModifiers/ModifierScripts/Projectile/Homing.cs
@@ -7,6 +7,7 @@ public class Homing : SpellModifier
 
     public float rotationSpeed = 2.0f;
     public Transform target = null;
+    Vector3 offset = Vector3.zero;
 
     Projectile pro;
 
@@ -17,6 +18,7 @@ public class Homing : SpellModifier
         if (target == null)
         {
             target = FindClosestTarget();
+            offset = target.tag == "Enemy" ? target.GetComponent<CapsuleCollider>().center : target.GetComponent<CharacterController>().center;
         }
     }
 
@@ -24,7 +26,7 @@ public class Homing : SpellModifier
     {
         float step = rotationSpeed * Time.deltaTime;
 
-        Vector3 targetDir = target.transform.position - transform.position;
+        Vector3 targetDir = target.transform.position + offset - transform.position;
         Vector3 newDir = Vector3.RotateTowards(pro.direction, targetDir, step, 0.0f);
 
         pro.direction = newDir;
@@ -33,7 +35,16 @@ public class Homing : SpellModifier
     private Transform FindClosestTarget()
     {
         GameObject[] gos;
-        gos = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if (pro.caster.tag == "Enemy")
+        {
+            gos = GameObject.FindGameObjectsWithTag("Player");
+        }
+        else
+        {
+            gos = GameObject.FindGameObjectsWithTag("Enemy");
+        }
+        
         GameObject closest = null;
         float distance = Mathf.Infinity;
         Vector3 position = transform.position;
