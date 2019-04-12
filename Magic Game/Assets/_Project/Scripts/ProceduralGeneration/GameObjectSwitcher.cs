b67ae310418cd477cf092894e587569f8e01342d
TuomoHei <31925862+TuomoHei@users.noreply.github.com>
2019-04-12 09:02:50 +0300

    GameObjectSwitcher-script
    Used for doorways but can be also used somewhere else... maybe...

---
+++ Magic Game/Assets/_Project/Scripts/ProceduralGeneration/GameObjectSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject a, b;
+
+    //a = wall
+    //b = doorway
+
+    void Update()
+    {
+        if (a.activeInHierarchy)
+        {
+            b.SetActive(false);
+        }
+
+        if (!a.activeInHierarchy)
+        {
+            b.SetActive(true);
+        }
+    }
+}
