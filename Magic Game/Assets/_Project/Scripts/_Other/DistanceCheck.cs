d3123f6d8df97c4340fea69e1d592622e8f22a2e
TuomoHei <31925862+TuomoHei@users.noreply.github.com>
2019-07-02 11:39:24 +0300

    scripts
    - CustomOcclusionCulling
    - DistanceCheck
    - LightFlicker

---
+++ Magic Game/Assets/_Project/Scripts/_Other/DistanceCheck.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceCheck : MonoBehaviour
+{
+    [SerializeField]
+    private float distance;
+
+    [SerializeField]
+    private GameObject[] childrenToHide;
+
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        foreach (GameObject child in childrenToHide)
+        {
+            if (Vector3.Distance(player.transform.position, child.transform.position) < distance)
+            {
+                child.SetActive(true);
+            }
+
+            else
+            {
+                child.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Magic Game/Assets/_Project/Scripts/_Other/LightFlicker.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour
+{
+    public float maxReduction;
+    public float maxIncrease;
+    public float rateDamping;
+    public float strength;
+    public bool stopFlickering;
+    public bool controlEmission;
+
+    private Light lightSource;
+    private Material lightMaterial;
+    private Renderer rend;
+    private Color color;
+
+    private float defaultIntensity;
+    private float defaultRange;
+    private bool isFlickering;
+
+    private const string EMISSION_COLOR = "_EmissionColor";
+
+    public void Reset()
+    {
+        maxReduction = 0.2f;
+        maxIncrease = 0.2f;
+        rateDamping = 0.1f;
+        strength = 300;
+    }
+
+    public void Start()
+    {
+        lightSource = GetComponent<Light>();
+
+        if (controlEmission)
+        {
+            rend = GetComponentInParent<Renderer>();
+            lightMaterial = rend.material;
+            color = lightMaterial.GetColor(EMISSION_COLOR);
+        }
+
+        if (lightSource == null)
+        {
+            Debug.LogError("LightFlicker-script needs light source.");
+            return;
+        }
+
+        defaultIntensity = lightSource.intensity;
+        defaultRange = lightSource.range;
+
+        if (controlEmission)
+        {
+            StartCoroutine(DoFlicker());
+        }
+
+        else
+        {
+            StartCoroutine(DoFlickerWithoutEmission());
+        }
+    }
+
+    void Update()
+    {
+        if (!stopFlickering && !isFlickering)
+        {
+            if (controlEmission)
+            {
+                StartCoroutine(DoFlicker());
+            }
+
+            else
+            {
+                StartCoroutine(DoFlickerWithoutEmission());
+            }
+        }
+    }
+
+    private IEnumerator DoFlicker()
+    {
+        isFlickering = true;
+
+        while (!stopFlickering)
+        {
+            lightMaterial.SetColor(EMISSION_COLOR, color * Mathf.Lerp(0.25f, 10, Time.deltaTime));
+
+            lightSource.intensity = Mathf.Lerp(lightSource.intensity, Random.Range(defaultIntensity - maxReduction, defaultIntensity + maxIncrease), strength * Time.deltaTime);
+            lightSource.range = Mathf.Lerp(lightSource.range, Random.Range(defaultRange - maxReduction, defaultRange + maxIncrease), strength * Time.deltaTime);
+
+            yield return new WaitForSeconds(rateDamping);
+        }
+
+        isFlickering = false;
+
+    }
+
+    private IEnumerator DoFlickerWithoutEmission()
+    {
+        isFlickering = true;
+
+        while (!stopFlickering)
+        {
+            lightSource.intensity = Mathf.Lerp(lightSource.intensity, Random.Range(defaultIntensity - maxReduction, defaultIntensity + maxIncrease), strength * Time.deltaTime);
+            lightSource.range = Mathf.Lerp(lightSource.range, Random.Range(defaultRange - maxReduction, defaultRange + maxIncrease), strength * Time.deltaTime);
+
+            yield return new WaitForSeconds(rateDamping);
+        }
+
+        isFlickering = false;
+
+    }
+}
