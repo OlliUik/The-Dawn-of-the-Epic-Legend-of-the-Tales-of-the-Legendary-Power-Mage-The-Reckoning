30771420dd6d6ebb4783d693608484d2c8b1c51b
AJ-Ora <antti-jussioravainen@kamk.fi>
2019-09-12 15:39:34 +0300

    Added a small test script, and placed it in AJ-TestScene.

--- Magic Game/Assets/_Project/Scripts/PowerMage/Entity.cs
+++ Magic Game/Assets/_Project/Scripts/PowerMage/Entity.cs
@@ -1,37 +1,16 @@
-﻿using System.Collections.Generic;
+﻿//using System.Collections.Generic;
 using UnityEngine;
 
 namespace PowerMage
 {
     public abstract class Entity : MonoBehaviour
     {
-        [System.Flags]
-        public enum StatusEffects
-        {
-            None        = 0,
-            OnFire      = 1,
-            Confused    = 2,
-            Frozen      = 4,
-            Ragdolled   = 8
-        }
+        /* ----- Health and mana ----- */
 
-        [SerializeField] private int health = 100;
-        [SerializeField] private int mana = 100;
-
-        protected StatusEffects effects = StatusEffects.None;
+        #region HEALTH
         
-        public virtual bool GetEffect(StatusEffects se)
-        {
-            return (effects & se) == se;
-        }
-
-        public virtual void SetEffect(StatusEffects se, bool b)
-        {
-            if (GetEffect(se) != b)
-            {
-                effects = effects ^ se;
-            }
-        }
+        public int maxHealth = 100;
+        public int health { get; private set; } = 100;
 
         public void Hurt(int amount)
         {
@@ -50,9 +29,78 @@ namespace PowerMage
         public void Heal(int amount)
         {
             health += amount;
+
+            if (health > maxHealth)
+            {
+                health = maxHealth;
+            }
+
+            OnHeal();
+        }
+
+        #endregion
+
+        #region MANA
+
+        public int maxMana = 100;
+        public int mana { get; private set; } = 100;
+
+        public void UseMana(int amount)
+        {
+            mana -= amount;
+            if (mana < 0)
+            {
+                mana = 0;
+            }
+        }
+
+        #endregion
+
+        /* ----- Status effects ----- */
+
+        #region STATUS_EFFECTS
+
+        [System.Flags]
+        public enum StatusEffects
+        {
+            None = 0,
+            OnFire = 1,
+            Confused = 2,
+            Frozen = 4,
+            Ragdolled = 8
+        }
+        
+        public StatusEffects effects { get; private set; } = StatusEffects.None;
+        
+        public bool GetEffect(StatusEffects se)
+        {
+            return (effects & se) == se;
+        }
+
+        public void SetEffect(StatusEffects se, bool b)
+        {
+            if (GetEffect(se) != b)
+            {
+                effects = effects ^ se;
+            }
+        }
+
+        #endregion
+
+        /* ----- Virtual functions ----- */
+
+        public virtual void Awake()
+        {
+            health = maxHealth;
+            mana = maxMana;
         }
 
         public virtual void OnHurt()
+        {
+
+        }
+
+        public virtual void OnHeal()
         {
 
         }
---
+++ Magic Game/Assets/_Project/Scripts/PowerMage/TestScript.cs
@@ -0,0 +1,15 @@
+﻿public class TestScript : PowerMage.Entity
+{
+    public override void Awake()
+    {
+        base.Awake();
+
+        UnityEngine.Debug.Log("Confused is " + GetEffect(StatusEffects.Confused));
+        SetEffect(StatusEffects.Confused, false);
+        UnityEngine.Debug.Log("Confused is " + GetEffect(StatusEffects.Confused));
+        SetEffect(StatusEffects.Confused, true);
+        UnityEngine.Debug.Log("Confused is " + GetEffect(StatusEffects.Confused));
+
+        UnityEngine.Debug.Log("Health is: " + health);
+    }
+}
