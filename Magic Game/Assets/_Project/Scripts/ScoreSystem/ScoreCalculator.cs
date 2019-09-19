daa67fae9c6616427992f9f090af9e44a8bdc458
TuomoHei <31925862+TuomoHei@users.noreply.github.com>
2019-09-19 16:10:38 +0300

    Two ScoreSystem-scripts added

---
+++ Magic Game/Assets/_Project/Scripts/ScoreSystem/ScoreCalculator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCalculator : MonoBehaviour
+{
+    [SerializeField] private float defaultScore = 0f;       //Default score enemy type gives
+    [SerializeField] private float score = 0f;              //Score that will be send to score system
+    [SerializeField] private float currentMultiplier = 1f;  //Player's current multiplier
+
+    private float tempIncrease = 0f;                        //Temporary increment to the multiplier
+
+    [SerializeField] private GameObject player;
+    private ScoreSystem scoreSystem;
+
+    void Start()
+    {
+        scoreSystem = player.GetComponent<ScoreSystem>();
+    }
+
+    void Update()
+    {
+        CountScore();           //Count score
+        SendScore(score);       //Send counted score to score system
+    }
+
+    private void CountScore()
+    {
+        EnemyType();     //Check enemy's type
+
+        currentMultiplier = scoreSystem.multiplier;
+        currentMultiplier += AddMultiplier();
+        score = defaultScore * currentMultiplier;
+    }
+
+    private void SendScore(float newScore)
+    {
+        scoreSystem.score += newScore;
+        //Update multipliers
+    }
+
+    private void EnemyType()
+    {
+        //We need way to check enemy type
+        AddMultiplier();
+    }
+
+    private float AddMultiplier()
+    {
+        //We need way to check what effects where on enemy upon dying
+        return tempIncrease;
+    }
+}
---
+++ Magic Game/Assets/_Project/Scripts/ScoreSystem/ScoreSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    public float score = 0f;
+    public float multiplier = 1f;       //Permanent multiplier
+    public int killStreak = 0;
+    public int crystalsFound = 0;       //Updated when player finds crystal
+    public int segmentCount = 0;        //Updated when player enters brand new segment
+
+    
+}
