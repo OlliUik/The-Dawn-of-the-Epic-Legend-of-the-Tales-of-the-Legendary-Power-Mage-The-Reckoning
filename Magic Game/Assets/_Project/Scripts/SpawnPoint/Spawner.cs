97a4c54b933fd1114bbbd167bc427767731ecf2a
Puthikanon Non TIX18 <nonputhikanon@kamk.fi>
2019-09-20 14:24:15 +0300

    try simple spawn point for enemy :sparkles:

---
+++ Magic Game/Assets/_Project/Scripts/SpawnPoint/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour  
+{
+    [SerializeField] private List<GameObject> enemies;
+    [SerializeField] private float frequency;
+    
+    void Start()
+    {
+        StartCoroutine(SpawnEnememy());
+    }
+
+    IEnumerator SpawnEnememy()
+    {
+        while(true)
+        {
+            GameObject enemy = enemies[Random.Range(0, enemies.Count)];
+            Instantiate(enemy, GetComponent<Transform>());
+
+            yield return new WaitForSeconds(frequency);
+        }
+    }
+}
