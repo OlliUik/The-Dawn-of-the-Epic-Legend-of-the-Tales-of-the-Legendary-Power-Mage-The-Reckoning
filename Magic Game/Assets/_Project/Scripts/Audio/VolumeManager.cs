6d19a2df4f3e3924a60865dc0ba76d10f52c9620
TuomoHei <31925862+TuomoHei@users.noreply.github.com>
2019-10-25 12:29:52 +0300

    Fixed volume reset problem

--- Magic Game/Assets/_Project/Scripts/Audio/VolumeManager.cs
+++ Magic Game/Assets/_Project/Scripts/Audio/VolumeManager.cs
@@ -6,10 +6,16 @@ using UnityEngine.UI;
 
 public class VolumeManager : MonoBehaviour
 {
+    #region VARIABLES
+
     public AudioMixer mixer = null;
     public Slider sliderMaster = null;
     public Slider sliderMusic = null;
     public Slider sliderSfx = null;
+
+    #endregion
+
+    #region UNITY_FUNCTIONS
 
     private void Start()
     {
@@ -18,21 +24,27 @@ public class VolumeManager : MonoBehaviour
         sliderSfx.value = PlayerPrefs.GetFloat("SfxVolume", 1.0f);
     }
 
+    #endregion
+
+    #region CUSTOM_FUNCTIONS
+
     public void SetMasterLevel (float value)
     {
-        mixer.SetFloat("MasterVolume", Mathf.Log10(value) * 20);
+        mixer.SetFloat("MasterVolume", value);
         PlayerPrefs.SetFloat("MasterVolume", sliderMaster.value);
     }
 
     public void SetMusicLevel (float value)
     {
-        mixer.SetFloat("MusicVolume", Mathf.Log10(value) * 20);
+        mixer.SetFloat("MusicVolume", value);
         PlayerPrefs.SetFloat("MusicVolume", sliderMusic.value);
     }
 
     public void SetSfxLevel (float value)
     {
-        mixer.SetFloat("SfxVolume", Mathf.Log10(value) * 20);
+        mixer.SetFloat("SfxVolume", value);
         PlayerPrefs.SetFloat("SfxVolume", sliderSfx.value);
     }
+
+    #endregion
 }
