fcd13c6b7d8aa6c69f073ba0c760801cbbf7fa68
NonPN <non9441@gmail.com>
2019-11-07 17:18:55 +0200

    try editing flying navigation follower script :bug:

--- Magic Game/Assets/_Project/Scripts/Enemy/EnemyFlyingNavigationFollower.cs
+++ Magic Game/Assets/_Project/Scripts/Enemy/EnemyFlyingNavigationFollower.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class EnemyFlyingNavigationFollower : MonoBehaviour
 {
     [SerializeField] private float desiredHeight = 10.0f;
+    [SerializeField] private float maxFloatingRange = 1.2f;
     [SerializeField] private EnemyCore cEnemyCore = null;
+    private bool atDesiredHieght = false;
 
     private float currentHeight = 0.0f;
 
@@ -16,8 +18,33 @@ public class EnemyFlyingNavigationFollower : MonoBehaviour
 
     void Update()
     {
+        if(!atDesiredHieght)
+        {
+            TakeOff();
+        } else
+        {
+            Floating();
+        }
+    }
+
+    void TakeOff()
+    {
         Vector3 currentPosition = cEnemyCore.cNavigation.transform.position;
         currentHeight = Mathf.Lerp(currentHeight, cEnemyCore.cNavigation.transform.position.y + desiredHeight, Time.deltaTime);
+        currentPosition.y = currentHeight;
+
+        transform.position = currentPosition;
+        if(currentHeight.Equals(desiredHeight))
+        {
+            atDesiredHieght = true;
+        }
+    }
+
+    void Floating()
+    {
+        Vector3 currentPosition = cEnemyCore.cNavigation.transform.position;
+        float bRange = Random.Range(-maxFloatingRange, maxFloatingRange);
+        currentHeight = Mathf.Lerp(currentHeight, cEnemyCore.cNavigation.transform.position.y + bRange, Time.deltaTime);
         currentPosition.y = currentHeight;
 
         transform.position = currentPosition;
