36c74731c5be87c5ea629d973535e634285eba3f
TuomoHei <31925862+TuomoHei@users.noreply.github.com>
2019-11-20 11:10:04 +0200

    Small changes to scripts and prefabs

--- Magic Game/Assets/_Project/Scripts/ProceduralGeneration/GenerationLoop.cs
+++ Magic Game/Assets/_Project/Scripts/ProceduralGeneration/GenerationLoop.cs
@@ -4,20 +4,16 @@ using UnityEngine;
 
 public class GenerationLoop : MonoBehaviour
 {
-    public static GenerationLoop loop;
-
     public GameObject generator = null;
     public bool isGenerating = false;
 
-    private GameObject currentGenerator = null;
     [SerializeField] private GameObject player = null;
-    [SerializeField] private Vector3 startPos = Vector3.zero;
-    [SerializeField] private Vector3 startRot = Vector3.zero;
+    private GameObject currentGenerator = null;
+    private Vector3 startPos = Vector3.zero;
+    private Vector3 startRot = Vector3.zero;
 
     private void Start()
     {
-        loop = this;
-
         startPos = player.transform.position;
         startRot = player.GetComponent<ThirdPersonCamera>().lookDirection;
 
