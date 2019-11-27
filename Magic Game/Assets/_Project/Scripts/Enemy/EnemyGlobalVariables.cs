51106eaea20e192ad030c7b0a813919badb2b3ba
Theeruth Borisuth <theeruth.b@ku.th>
2019-11-27 13:51:14 +0200

    add an unused gloabal variable as a backup

--- Magic Game/Assets/_Project/Scripts/Enemy/EnemyGlobalVariables.cs
+++ Magic Game/Assets/_Project/Scripts/Enemy/EnemyGlobalVariables.cs
@@ -6,15 +6,19 @@ public class EnemyGlobalVariables
 {
 
     private static readonly float healthRate = 20f;
+    private static readonly float damageRate = 2f;
     private static readonly int enemyIncreaseRate = 5;
 
+
     public static float enemyExtraHealth = 0f;
+    public static float enemyExtraDamage = 0;
     public static int extraEnemyAmount = 0;
     //public static float extraEnemyRate = 0f;
 
     public static void StageUp()
     {
         enemyExtraHealth += healthRate;
+        enemyExtraDamage += damageRate;
         extraEnemyAmount += enemyIncreaseRate;
     }
 
