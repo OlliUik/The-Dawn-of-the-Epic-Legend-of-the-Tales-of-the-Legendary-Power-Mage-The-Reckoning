8f1ff995522858c79a5eaabda77c903d3498e508
Theeruth Borisuth <theeruth.b@ku.th>
2019-11-28 10:46:08 +0200

    edit and comment some script

--- Magic Game/Assets/_Project/Scripts/_Other/DistanceCheck.cs
+++ Magic Game/Assets/_Project/Scripts/_Other/DistanceCheck.cs
@@ -5,20 +5,16 @@ using UnityEngine.AI;
 
 public class DistanceCheck : MonoBehaviour
 {
-    [SerializeField] private float distance = 0.0f;
     [SerializeField] private List<GameObject> childrenToHide = null;
-    //[SerializeField] private NavMeshSurface surface;
     [SerializeField] private GameObject disableBlock;
     [SerializeField] private GameObject generator;
 
-    
+
     [SerializeField] private float spawnDistanceLimitX = 0.0f;
     [SerializeField] private float spawnDistanceLimitY = 0.0f;
     [SerializeField] private float spawnDistanceLimitZ = 0.0f;
-  
 
     private LevelGenerator gen;
-
     private GameObject player = null;
 
     private void Start()
@@ -27,9 +23,9 @@ public class DistanceCheck : MonoBehaviour
         generator = GameObject.FindGameObjectWithTag("levelbuilder");
         foreach (Transform child in transform)
         {
-            if(child.tag.Equals("disableBlock"))
+            if (child.tag.Equals("disableBlock"))
             {
-                disableBlock = child.gameObject;            
+                disableBlock = child.gameObject;
             }
         }
         foreach (Transform child in transform)
@@ -43,78 +39,53 @@ public class DistanceCheck : MonoBehaviour
         gen = FindObjectOfType<LevelGenerator>();
     }
 
-    /*
     public virtual void OnDrawGizmos()
     {
-
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, distance);
-
-    }
-    */
-
-    public virtual void OnDrawGizmos()
-    {
-
-        
         Gizmos.color = Color.blue;
-        Gizmos.DrawLine(transform.position - new Vector3(spawnDistanceLimitX,0,0) , transform.position + new Vector3(spawnDistanceLimitX, 0, 0));
+        Gizmos.DrawLine(transform.position - new Vector3(spawnDistanceLimitX, 0, 0), transform.position + new Vector3(spawnDistanceLimitX, 0, 0));
         Gizmos.DrawLine(transform.position - new Vector3(0, spawnDistanceLimitY, 0), transform.position + new Vector3(0, spawnDistanceLimitY, 0));
         Gizmos.DrawLine(transform.position - new Vector3(0, 0, spawnDistanceLimitZ), transform.position + new Vector3(0, 0, spawnDistanceLimitZ));
-       
-      
-
     }
 
     private void Update()
-    {   
-        if(generator != null && player != null && childrenToHide != null)
+    {
+        if (generator != null && player != null && childrenToHide != null)
         {
             foreach (GameObject child in childrenToHide)
             {
                 if (gen.isDone)
                 {
-                    //if (Vector3.Distance(player.transform.position, child.transform.position) < distance)
                     CheckDistance(child);
-                    /*
-                    {
-                        child.SetActive(true);
-                        disableBlock.SetActive(false);
-                    }
-                    else
-                    {
-                        child.SetActive(false);
-                        disableBlock.SetActive(true);
-                    }
-                    */
                 }
 
             }
         }
-      
-       
+
+
     }
 
 
-   
-   private void CheckDistance(GameObject child)
-   {
-        if((Mathf.Abs(player.transform.position.x - child.transform.position.x) <= spawnDistanceLimitX) &&
+
+    private void CheckDistance(GameObject child)
+    {
+        if ((Mathf.Abs(player.transform.position.x - child.transform.position.x) <= spawnDistanceLimitX) &&
           (Mathf.Abs(player.transform.position.y - child.transform.position.y) <= spawnDistanceLimitY) &&
           (Mathf.Abs(player.transform.position.z - child.transform.position.z) <= spawnDistanceLimitZ))
-       {
+        {
+            /*
             Debug.Log(transform.position);
             Debug.Log("------------------ " + Mathf.Abs(transform.position.x - child.transform.position.x) + "," + Mathf.Abs(transform.position.y - child.transform.position.y) + "," + Mathf.Abs(transform.position.z - child.transform.position.z));
             Debug.Log("++++++++++++++++++ " + spawnDistanceLimitX + "," + spawnDistanceLimitY + "," + spawnDistanceLimitZ);
+            */
             child.SetActive(true);
             disableBlock.SetActive(false);
-       }
+        }
         else
         {
             child.SetActive(false);
             disableBlock.SetActive(true);
         }
-       
-   }
-  
+
+    }
+
 }
