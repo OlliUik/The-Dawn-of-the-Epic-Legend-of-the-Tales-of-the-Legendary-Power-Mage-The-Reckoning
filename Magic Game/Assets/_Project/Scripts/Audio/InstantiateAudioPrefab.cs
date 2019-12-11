a85df68377b57a3406121ea6d03724854bca1b13
Sumpp1 <35849820+Sumpp1@users.noreply.github.com>
2019-12-11 12:31:05 +0200

    Polish

---
+++ Magic Game/Assets/_Project/Scripts/Audio/InstantiateAudioPrefab.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateAudioPrefab : MonoBehaviour
+{
+    public GameObject audioprefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(audioprefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
