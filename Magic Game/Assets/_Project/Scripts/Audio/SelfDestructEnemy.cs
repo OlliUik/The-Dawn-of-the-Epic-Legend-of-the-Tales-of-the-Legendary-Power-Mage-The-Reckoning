7bb82b2748894a3d609c60abf75aa1e6a1d03994
Theeruth Borisuth <theeruth.b@ku.th>
2019-12-12 12:52:45 +0200

    enemy prefab now destroy in 4 seconds after death

---
+++ Magic Game/Assets/_Project/Scripts/Audio/SelfDestructEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructEnemy : MonoBehaviour
+{
+
+    [SerializeField] EnemyCore enemy;
+// Start is called before the first frame update
+     void Start()
+    {
+        enemy = GetComponentInParent<EnemyCore>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if(enemy == null)
+        {
+            Destroy(gameObject, 4); //5 is how many seconds you want before the object deletes itself
+        }
+    }
+}
