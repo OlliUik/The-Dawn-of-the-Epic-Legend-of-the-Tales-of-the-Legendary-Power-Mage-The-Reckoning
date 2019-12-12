f8bfb1fabe617fdf28fc56b5bb73a769aac21c75
NonPN <non9441@gmail.com>
2019-12-12 16:06:33 +0200

    fix credit scene to have a scroll smooth :bug:

--- Magic Game/Assets/_Project/Scripts/UI/CreditsScroller.cs
+++ Magic Game/Assets/_Project/Scripts/UI/CreditsScroller.cs
@@ -7,13 +7,17 @@ using UnityEngine.EventSystems;
 public class CreditsScroller : MonoBehaviour, IDragHandler, IEndDragHandler
 {
     private Vector2 contentPos;
+    private ScrollRect scrollRect;
+
     private float contentSize;
     private float startPos;
     private float viewPortSize;
-    private ScrollRect scrollRect;
+    private float waitCounter = 0;
+    private float cscrolleSpeed = 0;
 
     private bool isDraged = false;
     [SerializeField] private float scrollSpeed = 1;
+    [SerializeField] [Range(0,5)] private int waitTime;
     [SerializeField] private RectTransform contentBox;
 
     // Start is called before the first frame update
@@ -29,18 +33,30 @@ public class CreditsScroller : MonoBehaviour, IDragHandler, IEndDragHandler
     // Update is called once per frame
     void Update()
     {
-        if (!isDraged)
+        if (contentPos.y > (startPos + (contentSize + (viewPortSize))))
         {
-            contentPos = new Vector2(0f, contentPos.y + scrollSpeed);
+            contentPos = new Vector2(0f, startPos);
+            scrollRect.content.localPosition = contentPos;
+        }
+        else if (contentPos.y < startPos - viewPortSize)
+        {
+            contentPos = new Vector2(0f, startPos + (contentSize + viewPortSize));
             scrollRect.content.localPosition = contentPos;
         }
 
-        if (contentPos.y > (startPos + ((contentSize + viewPortSize) + 35)))
+        if (!isDraged)
         {
-            contentPos = new Vector2(0f, startPos);
-        } else if (contentPos.y < startPos)
-        {
-            contentPos = new Vector2(0f, startPos + ((contentSize + viewPortSize) - 35));
+            if (waitCounter > 0)
+            {
+                waitCounter -= Time.deltaTime;
+            } else
+            {
+                contentPos = scrollRect.content.localPosition;
+
+                cscrolleSpeed = Mathf.Lerp(cscrolleSpeed, scrollSpeed, Time.deltaTime);
+                contentPos = new Vector2(0f, contentPos.y + cscrolleSpeed);
+                scrollRect.content.localPosition = contentPos;
+            }
         }
     }
 
@@ -52,6 +68,8 @@ public class CreditsScroller : MonoBehaviour, IDragHandler, IEndDragHandler
     public void OnDrag(PointerEventData eventData)
     {
         isDraged = true;
+        waitCounter = waitTime;
+        cscrolleSpeed = 0;
         contentPos = scrollRect.content.localPosition;
     }
 
